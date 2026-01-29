17318a591be85f936e5e4cf19a479a0fd61b589e
osw6858 <osw7875@naver.com>
2026-01-29 23:12:39 +0900

    chore: nuqs 의존성 및 app-provider의 관련 어댑터 제거
    - package.json과 package-lock.json에서 nuqs 패키지 제거
    - AppProvider에서 NuqsAdapter를 제거

--- src/app/providers/app-provider.tsx
+++ src/app/providers/app-provider.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
-import { NuqsAdapter } from 'nuqs/adapters/react-router/v7';
 import { useState } from 'react';
 import { Toaster } from '@/shared/ui/sonner';
 
@@ -26,9 +25,7 @@ export function AppProvider({ children }: AppProviderProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <NuqsAdapter>{children}</NuqsAdapter>
-      </BrowserRouter>
+      <BrowserRouter>{children}</BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
       <Toaster position="top-center" />
     </QueryClientProvider>
